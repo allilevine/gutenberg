a4b7a25b048ffa0a542bebfe2c4a4942649f8955
Allison Levine <allison.levine@automattic.com>
2020-12-11 16:42:38 -0500

    Make fileId unique by using clientId instead of id

--- packages/block-library/src/file/edit.js
+++ packages/block-library/src/file/edit.js
@@ -49,7 +49,13 @@ function ClipboardToolbarButton( { text, disabled } ) {
 	);
 }
 
-function FileEdit( { attributes, setAttributes, noticeUI, noticeOperations } ) {
+function FileEdit( {
+	attributes,
+	setAttributes,
+	noticeUI,
+	noticeOperations,
+	clientId,
+} ) {
 	const {
 		id,
 		fileName,
@@ -92,6 +98,11 @@ function FileEdit( { attributes, setAttributes, noticeUI, noticeOperations } ) {
 			changeDownloadButtonText( _x( 'Download', 'button label' ) );
 		}
 	}, [] );
+
+	useEffect( () => {
+		// Add a unique fileId to each file block
+		setAttributes( { fileId: `wp-block-file--media-${ clientId }` } );
+	}, [ clientId ] );
 
 	function onSelectFile( newMedia ) {
 		if ( newMedia && newMedia.url ) {
